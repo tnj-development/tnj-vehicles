9bc818b8ed0a3d88d85199292e5c8b8055236fff
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-04-16 21:11:19 +0930

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,3 +4,8 @@ QBCore.Functions.CreateUseableItem("wheelchair", function(source)
     local source = source
     TriggerClientEvent('tnj-vehicles:wheelchair', source)
 end)
+
+QBCore.Functions.CreateUseableItem("hoverboard", function(source)
+    local source = source
+    TriggerClientEvent('tnj-vehicles:hoverboard', source)
+end)
